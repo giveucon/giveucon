988470a95cd2ba7e21a89e9dbb1d4187f389aaaf
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-03-25 06:15:34 +0900

    Create link with Kakao OAuth2 API

---
+++ backend/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Post
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'dark_mode')
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,17 @@
-from django.urls import path
-from .views import hello
+from django.urls import path, include
+from .views import *
+
 urlpatterns = [
+    # Hello world!
     path('hello/', hello),
+
+    # Login
+    path('account/login/', include('rest_auth.urls')),
+    path('account/registration/', include('rest_auth.registration.urls')),
+    path('account/login/', include('allauth.urls')),
+    path('', include('django.contrib.auth.urls')),
+
+    # Social Login
+    path('account/login/kakao/', kakao_login, name='kakao_login'),
+    path('account/login/kakao/callback/', kakao_callback, name='kakao_callback'),
 ]
