7e6bb473db728963b493843fd45fc28d67aa98e6
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-03-26 13:33:38 +0900

    Create test page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import styled from 'styled-components'
+
+import Test from './test'
 
 const Title = styled.h1`
   color: green;
@@ -11,13 +10,6 @@ const Title = styled.h1`
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Title>Front-end</Title>
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Successfully launched
-        </Typography>
-      </Box>
-    </Container>
+    <Test />
   );
 }
