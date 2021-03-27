e289ed3dc40597cbf712bc00a958bde3e1a62d48
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-03-28 08:47:52 +0900

    Apply django-allauth authentication module

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,19 +1,7 @@
 from django.urls import path, include
-from .views import account_views, test_views
+from .views import test_views
 
-urlpatterns = [
-    # Login
-    # account_views.py
-    path('account/login/', include('rest_auth.urls')),
-    path('account/login/', include('allauth.urls')),
-    path('account/registration/', include('rest_auth.registration.urls')),
-    path('', include('django.contrib.auth.urls')),
-
-    # Social Login
-    # account_views.py
-    path('account/login/kakao/', account_views.kakao_login, name='kakao_login'),
-    path('account/login/kakao/callback/', account_views.kakao_callback, name='kakao_callback'),
-    
+urlpatterns = [    
     # Test
     # test_views.py
     path('test/hello/', test_views.hello),
