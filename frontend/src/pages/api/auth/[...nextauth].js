76807b01972beb8b171ae9868043cd223c75ff16
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-04-01 20:45:09 +0900

    Delete TypeScript files

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -2,10 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { InitOptions } from "next-auth";
 import Providers from "next-auth/providers";
 import axios from "axios";
-import { AuthenticatedUser } from "../../../../types";
 
-
-const settings: InitOptions = {
+const settings = {
   providers: [
     Providers.Kakao({
       clientId: process.env.KAKAO_CLIENT_ID,
@@ -13,11 +11,12 @@ const settings: InitOptions = {
     }),
   ],
   callbacks: {
-    async signIn(user: AuthenticatedUser, account, profile) {
+    async signIn(user, account, profile) {
       // may have to switch it up a bit for other providers
       if (account.provider === "kakao") {
         // extract these two tokens
-        const { accessToken, idToken } = account;
+        const { accessToken } = account;
+        console.log("19 accessToken: " + accessToken);
     
         // make a POST request to the DRF backend
         try {
@@ -26,40 +25,43 @@ const settings: InitOptions = {
             "http://127.0.0.1:8000/api/social/login/kakao/",
             {
               access_token: accessToken, // note the differences in key and value variable names
-              id_token: idToken,
             },
           );
     
           // extract the returned token from the DRF backend and add it to the `user` object
           const { access_token } = response.data;
+          console.log("33 accessToken: " + access_token);
           user.accessToken = access_token;
     
           return true; // return true if everything went well
         } catch (error) {
+          console.log(error);
           return false;
         }
       }
       return false;
     },
     
-    async jwt(token, user: AuthenticatedUser, account, profile, isNewUser) {
+    async jwt(token, user, account, profile, isNewUser) {
       if (user) {
         const { accessToken } = user;
     
         // reform the `token` object from the access token we appended to the `user` object
         token.accessToken = accessToken;
+        console.log("51 token.accessToken: " + token.accessToken);
       }
     
       return token;
     },
     
-    async session(session, user: AuthenticatedUser) {
+    async session(session, user) {
       session.accessToken = user.accessToken;
+      console.log("59 session.accessToken: " + session.accessToken);
       return session;
     },
   },
 };
 
-export default (req: NextApiRequest, res: NextApiResponse) =>
+export default (req, res) =>
   NextAuth(req, res, settings);
   
