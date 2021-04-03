2b6e5d96136f9f6d02f8f749f17938a3ac51d47c
Byeonggon Lee <gonny952@gmail.com>
2021-04-04 03:21:43 +0900

    Change current-user to users/current

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
 from .views import *
 
-
 urlpatterns = [
     # Social views
     path("social/login/kakao/", SocialKakaoLoginView.as_view()),
 
     # User views
-    path('current-user/', CurrentUserDetailView.as_view()),
     path('users/', UserListView.as_view()),
-    path('users/<str:pk>/', UserDetailView.as_view()),
+    path('users/current', CurrentUserDetailView.as_view()),
+    path('users/<int:pk>/', UserDetailView.as_view()),
 
     # Test views
     path('ping/', PingView.ping),
--- backend/api/views/user_list_view.py
+++ backend/api/views/user_list_view.py
@@ -4,6 +4,6 @@ from rest_framework import generics
 from ..models import User
 from ..serializers import UserSerializer
 
-class UserListView(generics.ListCreateAPIView ):
+class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
