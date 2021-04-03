ad6bd8e04b2e332a9b495cd773f96c8680c466e5
Byeonggon Lee <gonny952@gmail.com>
2021-04-04 01:07:10 +0900

    Temporarily fix drf error

--- backend/giveucon/settings.py
+++ backend/giveucon/settings.py
@@ -85,7 +85,8 @@ REST_USE_JWT = True # use JSON Web Tokens
 # JWT_AUTH_COOKIE = "nextjsdrf-access-token"
 # JWT_AUTH_REFRESH_COOKIE = "nextjsdrf-refresh-token"
 # JWT_AUTH_SAMESITE = "none"
-SOCIALACCOUNT_AUTO_SIGNUP = False
+
+#SOCIALACCOUNT_AUTO_SIGNUP = False
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
