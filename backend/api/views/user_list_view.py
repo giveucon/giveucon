881367b55bfc101d3664bc6c9ee1dd4f8e1b2e38
Byeonggon Lee <gonny952@gmail.com>
2021-04-04 22:25:11 +0900

    Fix user creation

--- backend/api/views/user_list_view.py
+++ backend/api/views/user_list_view.py
@@ -1,10 +1,17 @@
 from django.utils import timezone
 from rest_framework import generics
+from rest_framework.response import Response
 
 from ..models import User
+from ..models import AccountUser
 from ..serializers import UserSerializer
 
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-#    def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        AccountUser.objects.create(account=request.user, user=user)
+        return Response(serializer.data)
