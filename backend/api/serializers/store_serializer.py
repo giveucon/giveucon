9955d8230b7041a1d7dbac1be855a94d471d9296
Byeonggon Lee <gonny952@gmail.com>
2021-04-05 00:25:21 +0900

    Generate private / public key when creating new store

--- backend/api/serializers/store_serializer.py
+++ backend/api/serializers/store_serializer.py
@@ -1,8 +1,18 @@
+import ecdsa
+from django.conf import settings
+from hashlib import sha256
 from rest_framework.serializers import ModelSerializer
 from ..models import Store
-from django.conf import settings
 
 class StoreSerializer(ModelSerializer):
     class Meta:
         model = Store
-        fields = '__all__'
+        exclude = ('private_key', 'public_key')
+    def create(self, validated_data):
+        private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=sha256)
+        public_key = private_key.get_verifying_key()
+        store = Store(**validated_data)
+        store.private_key = private_key.to_string().hex()
+        store.public_key = public_key.to_string().hex()
+        store.save()
+        return store
--- backend/api/views/store_list_view.py
+++ backend/api/views/store_list_view.py
@@ -6,13 +6,3 @@ from ..serializers import StoreSerializer
 class StoreListView(generics.ListCreateAPIView):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-    '''
-    def create(self, request, *args, **kwargs):
-        #serializer = self.get_serializer(data=request.data)
-        #serializer.is_valid(raise_exception=True)
-        #store = serializer.save()
-        #return Response(serializer.data)
-        request.data['private_key'] = 'abcd'
-        request.data['public_key'] = 'abcd'
-        super(self, request, *args, **kwargs)
-    '''
