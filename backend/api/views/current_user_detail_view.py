dc0bde9cd0f551d52f7e83c7bc393150cc4a70ab
Byeonggon Lee <gonny952@gmail.com>
2021-04-04 22:31:36 +0900

    Add user update

--- backend/api/views/current_user_detail_view.py
+++ backend/api/views/current_user_detail_view.py
@@ -8,8 +8,15 @@ from ..models import AccountUser
 from ..serializers import UserSerializer
 
 class CurrentUserDetailView(generics.RetrieveUpdateDestroyAPIView):
-#    queryset = User.objects.all()
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     def retrieve(self, request, *args, **kwargs):
         user = get_object_or_404(AccountUser, account=request.user).user
         return Response(self.get_serializer(user).data)
+
+    def update(self, request, *args, **kwargs):
+        user = get_object_or_404(AccountUser, account=request.user).user
+        serializer = self.get_serializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
