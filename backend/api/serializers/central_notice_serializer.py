d14a14827893a2516f0978a37da2a55f1b7cd457
Byeonggon Lee <gonny952@gmail.com>
2021-04-09 13:21:07 +0900

    Add central notice view

--- backend/api/serializers/central_notice_serializer.py
+++ backend/api/serializers/central_notice_serializer.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
-from ..models import CentralNotice
+from . import ArticleSerializer
+from ..models import Article, CentralNotice
 from django.conf import settings
 
 class CentralNoticeSerializer(ModelSerializer):
+    article = ArticleSerializer()
     class Meta:
         model = CentralNotice
         fields = '__all__'
+    def create(self, validated_data):
+        article_data = validated_data.pop('article')
+        user = validated_data.pop('user')
+        article = Article.objects.create(user=user, **article_data)
+        central_notice = CentralNotice.objects.create(article=article, **validated_data)
+        return central_notice
--- backend/api/views/central_notice_list_view.py
+++ backend/api/views/central_notice_list_view.py
@@ -1,9 +1,17 @@
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.response import Response
 
-from ..models import CentralNotice
+from ..models import AccountUser, CentralNotice
 from ..serializers import CentralNoticeSerializer
 
 class CentralNoticeListView(generics.ListCreateAPIView):
     queryset = CentralNotice.objects.all()
     serializer_class = CentralNoticeSerializer
+    def create(self, request, *args, **kwargs):
+        user = get_object_or_404(AccountUser, account=request.user).user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        return Response(serializer.data)
