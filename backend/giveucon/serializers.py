f160ae1f0c4f0fe3badb4ac9d78b4d4dbaa91930
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-04-21 10:35:25 +0900

    Test OAuth requests

--- backend/giveucon/serializers.py
+++ backend/giveucon/serializers.py
@@ -7,10 +7,10 @@ class TokenObtainLifetimeSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
         refresh = self.get_token(self.user)
-        data['access_token_lifetime'] = int(refresh.access_token.lifetime.total_seconds())
-        data['refresh_token_lifetime'] = int(refresh.lifetime.total_seconds())
-        data['access_token_expiry'] = str(datetime.now() + refresh.access_token.lifetime)
-        data['refresh_token_expiry'] = str(datetime.now() + refresh.lifetime)
+        data['access_lifetime'] = int(refresh.access_token.lifetime.total_seconds())
+        data['refresh_lifetime'] = int(refresh.lifetime.total_seconds())
+        data['access_expiry'] = str(datetime.now() + refresh.access_token.lifetime)
+        data['refresh_expiry'] = str(datetime.now() + refresh.lifetime)
         return data
 
 class TokenRefreshLifetimeSerializer(TokenRefreshSerializer):
