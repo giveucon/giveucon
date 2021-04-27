d6ed36772294bb344b7bc30aedcc2f4a54d65ec7
Byeonggon Lee <gonny952@gmail.com>
2021-04-27 19:05:42 +0900

    Fix mixin

--- backend/api/mixins/serializer_mixin.py
+++ backend/api/mixins/serializer_mixin.py
@@ -1,26 +1,27 @@
 class SerializerMixin(object):
     serializer_class = None
-    create_serializer_class = None
-    retrieve_serializer_class = None
-    list_serializer_class = None
-    update_serializer_class = None
-    partial_update_serializer_class = None
-    destroy_update_serializer_class = None
-
-    serializer_class_mapper = {
-        'create': create_serializer_class,
-        'retrieve': retrieve_serializer_class,
-        'list': list_serializer_class,
-        'update': update_serializer_class,
-        'partial_update': partial_update_serializer_class,
-        'destroy': destroy_update_serializer_class
-    }
+    serializer_class_read = None
+    serializer_class_write = None
+    serializer_class_get = None
+    serializer_class_post = None
+    serializer_class_put = None
 
     def get_serializer_class(self):
-        serializer_class = self.serializer_class_mapper[self.action]
+        print('get_serializer_class()')
+
+        serializer_class_mapper = {
+            'GET': self.serializer_class_get or self.serializer_class_read,
+            'POST': self.serializer_class_post or self.serializer_class_write,
+            'PUT': self.serializer_class_put or self.serializer_class_write,
+        }
+
+        serializer_class = serializer_class_mapper[self.request.method]
+
         if serializer_class is None:
             serializer_class = self.serializer_class
+
         assert serializer_class is not None, (
             f'serializer for ${self.action} action is not defined'
         )
+
         return serializer_class
