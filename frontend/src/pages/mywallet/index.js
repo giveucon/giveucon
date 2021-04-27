5103ed72cd2bb48d62aaac2468dc4e78faa7435a
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-04-27 19:34:03 +0900

    Fix typo

--- frontend/src/pages/mywallet/index.js
+++ frontend/src/pages/mywallet/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router'
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
--- frontend/src/pages/products/charge.js
+++ frontend/src/pages/products/charge.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router'
 import toast from 'react-hot-toast';
 import Box from '@material-ui/core/Box';
