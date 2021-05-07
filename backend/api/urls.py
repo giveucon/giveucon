e48dfc742dedb8ca9e2f7cbf7a4d075d8747e3c8
Byeonggon Lee <gonny952@gmail.com>
2021-05-07 21:43:26 +0900

    Fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -50,13 +50,13 @@ urlpatterns = [
     path('product-reviews/<int:pk>/', ProductReviewDetailView.as_view()),
 
     path('favorite-stores/', FavoriteStoreListView.as_view()),
-    path('favorite-stores/<int:pk>', FavoriteStoreDetailView.as_view()),
+    path('favorite-stores/<int:pk>/', FavoriteStoreDetailView.as_view()),
 
     path('favorite-products/', FavoriteProductListView.as_view()),
-    path('favorite-products/<int:pk>', FavoriteProductDetailView.as_view()),
+    path('favorite-products/<int:pk>/', FavoriteProductDetailView.as_view()),
 
     path('friends/', FriendListView.as_view()),
-    path('friends/<int:pk>', FriendDetailView.as_view()),
+    path('friends/<int:pk>/', FriendDetailView.as_view()),
 
     path('dummy-users/', DummyUserCreateView.as_view()),
     path('dummy-stores/', DummyStoreCreateView.as_view()),
