365b9275ca2cda801c3dfda1545939bfe2af3f36
Byeonggon Lee <gonny952@gmail.com>
2021-05-07 21:59:31 +0900

    Add filter and pagination to user

--- backend/api/views/user_list_view.py
+++ backend/api/views/user_list_view.py
@@ -3,13 +3,15 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 from ..models import User
-
 from ..models import AccountUser
+from ..paginations import UserPagination
 from ..serializers import UserSerializer
 
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = UserPagination
+    filterset_fields = ['user_name', 'email']
 
     def perform_create(self, serializer):
         with transaction.atomic():
