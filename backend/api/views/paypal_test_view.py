2813167c11598f4d32d3a97a5931f31dcf9ad714
Byeonggon Lee <gonny952@gmail.com>
2021-05-14 11:56:39 +0900

    Modify paypal test view

--- backend/api/views/paypal_test_view.py
+++ backend/api/views/paypal_test_view.py
@@ -3,16 +3,19 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from giveucon.secrets import DJANGO_PAYPAL_CLIENT_ID
 from giveucon.secrets import DJANGO_PAYPAL_CLIENT_SECRET
+from base64 import b64encode
 
 class PayPalTestView(APIView):
     def get(self, request):
+        authorization = 'Basic ' + b64encode(f'{DJANGO_PAYPAL_CLIENT_ID}:{DJANGO_PAYPAL_CLIENT_SECRET}'.encode()).decode()
+        print(authorization)
         res = requests.post(
             'https://api-m.sandbox.paypal.com/v1/oauth2/token',
             {'grant_type': 'client_credentials'},
             {
                 'Accept': 'application/json',
                 'Accept-Language': 'en_US',
-                'Authorization': f'Basic {DJANGO_PAYPAL_CLIENT_ID}:{DJANGO_PAYPAL_CLIENT_SECRET}',
+                'Authorization': authorization,
             }
         )
         return Response(res)
