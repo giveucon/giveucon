6885f190250f6e32ae7af6ec601a0b05e625f09d
Byeonggon Lee <gonny952@gmail.com>
2021-05-15 08:48:44 +0900

    Make validation_code optional for superuser

--- backend/api/serializers/user_write_serializer.py
+++ backend/api/serializers/user_write_serializer.py
@@ -6,29 +6,42 @@ from ..services import PhoneService
 from .user_read_serializer import UserReadSerializer
 
 class UserWriteSerializer(serializers.ModelSerializer):
-    verification_code = serializers.CharField()
+    verification_code = serializers.CharField(required=False)
     class Meta:
         model = User
         read_only_fields = ('staff',)
         fields = '__all__'
 
     def validate(self, data):
-        if self.context['request'].user.is_superuser:
-            return data
-        phone_number = data['phone_number']
-        verification_code = data['verification_code']
-        if not PhoneService.verify_phone_number(phone_number, verification_code):
-            raise serializers.ValidationError({'verification_code': 'Invalid verification code'})
+
         return data
 
     def create(self, validated_data):
         with transaction.atomic():
-            validated_data.pop('verification_code')
+            validation_code = validated_data.pop('verification_code', None)
+            if not self.context['request'].user.is_superuser:
+                phone_number = validated_data['phone_number']
+                if not PhoneService.verify_phone_number(phone_number, verification_code):
+                    raise serializers.ValidationError({
+                        'verification_code': 'Invalid verification code'
+                    })
             account = validated_data.pop('account')
-            print('account is ', account)
             user = User.objects.create(**validated_data)
             AccountUser.objects.create(account=account, user=user)
             return user
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            validation_code = validated_data.pop('verification_code', None)
+            phone_number = validated_data['phone_number']
+            if instance.phone_number != phone_number:
+                if (not self.context['request'].user.is_superuser
+                    and not PhoneService.verify_phone_number(phone_number, verification_code)):
+                    raise serializers.ValidationError({
+                        'verification_code': 'Invalid verification code'
+                    })
+
+            return super().update(instance, validated_data)
 
     def to_representation(self, instance):
         return UserReadSerializer(instance).data
