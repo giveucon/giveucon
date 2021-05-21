840d73f684fae20ee84ad48a682736b22a2f86e4
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-05-21 11:06:09 +0900

    Remove duplicated code

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -42,7 +42,6 @@ class MyDocument extends Document {
             content={lightTheme.palette.primary.main}
           />
           <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-          <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
           <meta name='description' content='Description' />
           <meta name='keywords' content='Keywords' />
           <link rel='apple-touch-icon' sizes='57x57' href='/images/icons/apple-icon-57x57.png' />
