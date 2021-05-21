9c35297639a9321b4791da5b8efa5d19575e4f4a
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-05-21 10:55:40 +0900

    Fix typo

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -45,26 +45,26 @@ class MyDocument extends Document {
           <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
           <meta name='description' content='Description' />
           <meta name='keywords' content='Keywords' />
-          <link rel="apple-touch-icon" sizes="57x57" href="/images/icons/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/images/icons/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/images/icons/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/images/icons/apple-icon-76x76.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/images/icons/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/images/icons/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192"  href="/images/icons/android-icon-192x192.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/images/icons/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="msapplication-TileColor" content="#2e7031" />
-          <meta name="msapplication-TileImage" content="/images/icons/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#43a047" />
+          <link rel='apple-touch-icon' sizes='57x57' href='/images/icons/apple-icon-57x57.png' />
+          <link rel='apple-touch-icon' sizes='60x60' href='/images/icons/apple-icon-60x60.png' />
+          <link rel='apple-touch-icon' sizes='72x72' href='/images/icons/apple-icon-72x72.png' />
+          <link rel='apple-touch-icon' sizes='76x76' href='/images/icons/apple-icon-76x76.png' />
+          <link rel='apple-touch-icon' sizes='114x114' href='/images/icons/apple-icon-114x114.png' />
+          <link rel='apple-touch-icon' sizes='120x120' href='/images/icons/apple-icon-120x120.png' />
+          <link rel='apple-touch-icon' sizes='144x144' href='/images/icons/apple-icon-144x144.png' />
+          <link rel='apple-touch-icon' sizes='152x152' href='/images/icons/apple-icon-152x152.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/images/icons/apple-icon-180x180.png' />
+          <link rel='icon' type='image/png' sizes='192x192'  href='/images/icons/android-icon-192x192.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/images/icons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='96x96' href='/images/icons/favicon-96x96.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/images/icons/favicon-16x16.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <meta name='msapplication-TileColor' content='#2e7031' />
+          <meta name='msapplication-TileImage' content='/images/icons/ms-icon-144x144.png' />
+          <meta name='theme-color' content='#43a047' />
           <link
             rel='stylesheet'
-            href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap"
+            href='https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap'
           />
           <script src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JAVASCRIPT_KEY}&autoload=false&libraries=services`}></script>
         </Head>
