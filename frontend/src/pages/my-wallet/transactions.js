5106aca953db1efc6ace634eab102ec5c8ad5756
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-05-26 06:40:50 +0900

    Add chart to transaction page

--- frontend/src/pages/my-wallet/transactions.js
+++ frontend/src/pages/my-wallet/transactions.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router'
+import { ResponsiveStream } from '@nivo/stream'
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
 import AlertBox from 'components/AlertBox'
@@ -10,29 +12,91 @@ import useI18n from 'hooks/useI18n'
 import request from 'utils/request'
 import withAuthServerSideProps from 'utils/withAuthServerSideProps'
 
-const getSelfBlockchainTransactionList = async (selfUser) => await request(
+const getSelfBlockchainAccount = async (selfUser) => await request(
   {
-    url: `https://blockstream.info/api/address/${selfUser.wallet}/txs`,
+    url: `https://blockstream.info/api/address/${selfUser.wallet}`,
     method: 'get'
   }
 );
 
+const getSelfBlockchainTransactionList = async (selfUser) => await request({
+  url: `https://blockstream.info/api/address/${selfUser.wallet}/txs`,
+  method: 'get'
+});
+
 export const getServerSideProps = withAuthServerSideProps (async (context, lng, lngDict, selfUser) => {
+  const getSelfBlockchainAccountResponse = await getSelfBlockchainAccount(selfUser);
   const getSelfBlockchainTransactionListResponse = await getSelfBlockchainTransactionList(selfUser);
   return {
     props: {
       lng,
       lngDict,
       selfUser,
+      selfBlockchainAccount: getSelfBlockchainAccountResponse.data,
       selfBlockchainTransactionList: getSelfBlockchainTransactionListResponse.data,
     }
   }
 })
 
-function Transactions({ lng, lngDict, selfUser, selfBlockchainTransactionList }) {
+function Transactions({ lng, lngDict, selfUser, selfBlockchainAccount, selfBlockchainTransactionList }) {
 
   const i18n = useI18n();
   const router = useRouter();
+  const depositWithdrawalData = [
+    {vout: [{value: selfBlockchainAccount.chain_stats.funded_txo_sum - selfBlockchainAccount.chain_stats.spent_txo_sum}]},
+    ...selfBlockchainTransactionList
+  ].map((element, index) => {
+    if (index === 0) return {
+      vin: element.vin,
+      vout: element.vout,
+      variant: 'current',
+      timestamp: null,
+      fee: null
+    };
+    else return {
+      vin: element.vin,
+      vout: element.vout,
+      variant: element.vout.map(lhs => lhs.scriptpubkey_address).includes(selfUser.wallet) ? 'deposit' : 'withdraw',
+      timestamp: element.status.block_time,
+      fee: element.fee
+    }
+  })
+  .map(element => {
+    if (element.variant === 'deposit') return {
+      value: element.vout.find(lhs => lhs.scriptpubkey_address === selfUser.wallet).value,
+      variant: element.variant,
+      timestamp: element.timestamp,
+      fee: element.fee
+    };
+    else if (element.variant === 'withdraw') return {
+      value: element.vin.find(lhs => lhs.prevout.scriptpubkey_address === selfUser.wallet).prevout.value,
+      variant: element.variant,
+      timestamp: element.timestamp,
+      fee: element.fee
+    };
+    else return {
+      value: element.vout.reduce((lhs, rhs) => {return lhs.value + rhs.value}).value,
+      variant: element.variant,
+      timestamp: element.timestamp,
+      fee: element.fee
+    };
+  });
+
+  const balanceData = depositWithdrawalData.map((element, index, array) => {
+    if (element.variant === 'current') return {value: element};
+    else return {
+      ...element,
+      value: array.slice(0, index + 1).reduce((lhs, rhs) => {
+        console.log(lhs);
+        return rhs.variant === 'deposit' ? {value: lhs.value - rhs.value} : {value: lhs.value + rhs.value}
+      })
+    }
+  }).map(element => element.value)
+  .reverse();
+
+  console.log(depositWithdrawalData);
+  console.log(balanceData);
+
 
   return (
     <Layout
@@ -45,15 +109,36 @@ function Transactions({ lng, lngDict, selfUser, selfBlockchainTransactionList })
         backButton
         title={i18n.t('myTransactions')}
       >
-        {selfBlockchainTransactionList && (selfBlockchainTransactionList.length > 0) ? (
+
+        <Box style={{height: 200}}>
+          <ResponsiveStream
+            data={balanceData}
+            keys={[ 'value' ]}
+            margin={{ top: 25, right: 25, bottom: 25, left: 25 }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                orient: 'bottom',
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0
+            }}
+            axisLeft={null}
+            enableGridY={true}
+            offsetType="none"
+            colors={{ scheme: 'accent' }}
+          />
+        </Box>
+
+        {depositWithdrawalData && (depositWithdrawalData.length > 0) ? (
           <Grid container spacing={1}>
-            {selfBlockchainTransactionList.map((item) => (
+            {depositWithdrawalData.slice(-(depositWithdrawalData.length - 1)).map((item) => (
               <Grid item xs={12}>
                 <TransactionListItem
-                  variant={item.vout.map(element => element.scriptpubkey_address).includes(selfUser.wallet) ? 'deposit' : 'withdraw'}
-                  amount={item.vout.map(element => element.value).reduce((lhs, rhs) => lhs + rhs)}
+                  variant={item.variant}
+                  amount={item.value}
                   fee={item.fee}
-                  timestamp={item.status.block_time}
+                  timestamp={item.timestamp}
                   unit={i18n.t('_currencyBTC')}
                   onClick={() => router.push(`https://www.blockchain.com/${'btc'}/tx/${item.txid}/`)}
                 />
