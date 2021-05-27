8b95064a12e47a8690b8e4a4b7a9e858a5d048ad
binkoni <gonny952@gmail.com>
2021-05-27 18:42:16 +0900

    Fix image serializer

--- backend/api/serializers/image_serializer.py
+++ backend/api/serializers/image_serializer.py
@@ -1,7 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from urllib.parse import urljoin
+from rest_framework import serializers
+from giveucon.settings import DJANGO_BASE_URL
 from ..models import Image
 
-class ImageSerializer(ModelSerializer):
+class ImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = Image
         fields = '__all__'
+    def get_image(self, instance):
+        return urljoin(DJANGO_BASE_URL, instance.image.url)
+
