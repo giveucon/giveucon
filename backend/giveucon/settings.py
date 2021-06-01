919aff43f33a502a17d1986a05518e163f2837f0
binkoni <gonny952@gmail.com>
2021-06-01 20:23:03 +0900

    Access token time

--- backend/giveucon/settings.py
+++ backend/giveucon/settings.py
@@ -101,7 +101,7 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=600),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True, 
