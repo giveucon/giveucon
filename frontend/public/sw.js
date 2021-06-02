f7bc74a3c0f854fc40c6227d222ab3c721424018
Kyeonghwan Son <snapperbay4453@gmail.com>
2021-06-02 20:42:23 +0900

    Fix PWA feature

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,3 +1,17 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   console.log("Hello world from the Service Worker");
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    try {
+      const res = await fetch(event.request);
+      const cache = await caches.open('cache');
+      cache.put(event.request.url, res.clone());
+      return res;
+    }
+    catch(error) {
+      return caches.match(event.request);
+    }
+  }());
 });
